069627da50a5cbab9fa8dd88ca6b061786a157c1
hashilnizam <hashilnizam@gmail.com>
2024-02-15 12:57:59 +0530

    slidebar mobile view issue solved

--- resources/views/user/common/app.blade.php
+++ resources/views/user/common/app.blade.php
@@ -3,7 +3,7 @@
   <head>
 
 
-      <link rel="icon" href="{{ asset('user/images/icon.png') }}" type="image/png">
+      <link rel="icon" href="{{ asset('/user/images/icon.png') }}" type="image/png">
       <title>Unseenstay</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,26 +11,26 @@
       <link href="{{ asset('https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700') }}" rel="stylesheet">
       <link href="{{ asset('https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i') }}" rel="stylesheet">
 
-      <link rel="stylesheet" href="{{ asset('user/css/open-iconic-bootstrap.min.css') }}">
-      <link rel="stylesheet" href="{{ asset('user/css/animate.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/open-iconic-bootstrap.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/animate.css') }}">
 
-      <link rel="stylesheet" href="{{ asset('user/css/owl.carousel.min.css') }}">
-      <link rel="stylesheet" href="{{ asset('user/css/owl.theme.default.min.css') }}">
-      <link rel="stylesheet" href="{{ asset('user/css/magnific-popup.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/owl.carousel.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/owl.theme.default.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/magnific-popup.css') }}">
 
-      <link rel="stylesheet" href="{{ asset('user/css/aos.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/aos.css') }}">
 
-      <link rel="stylesheet" href="{{ asset('user/css/ionicons.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/ionicons.min.css') }}">
 
-      <link rel="stylesheet" href="{{ asset('user/css/bootstrap-datepicker.css') }}">
-      <link rel="stylesheet" href="{{ asset('user/css/jquery.timepicker.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/bootstrap-datepicker.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/jquery.timepicker.css') }}">
 
-      <link rel="stylesheet" href="{{ asset('user/css/flaticon.css') }}">
-      <link rel="stylesheet" href="{{ asset('user/css/icomoon.css') }}">
-      <link rel="stylesheet" href="{{ asset('user/css/style.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/flaticon.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/icomoon.css') }}">
+      <link rel="stylesheet" href="{{ asset('/user/css/style.css') }}">
 
-{{--      <link rel="stylesheet" href="{{ asset('user/booking/css/bootstrap.min.css') }}">--}}
-      <link rel="stylesheet" href="{{ asset('user/booking/css/style.css') }}">
+{{--      <link rel="stylesheet" href="{{ asset('/user/booking/css/bootstrap.min.css') }}">--}}
+      <link rel="stylesheet" href="{{ asset('/user/booking/css/style.css') }}">
 
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
@@ -39,7 +39,31 @@
       <link rel="preconnect" href="https://fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css2?family=Futura:wght@400;700&display=swap" rel="stylesheet">
 
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 
+      <script>
+          $(document).ready(function(){
+              $('.home-slider.owl-carousel').owlCarousel({
+                  items: 1,
+                  loop: true,
+                  nav: true,
+                  dots: true,
+                  touchDrag: true,
+                  responsive:{
+                      0:{
+                          items:1
+                      },
+                      600:{
+                          items:1
+                      },
+                      1000:{
+                          items:1
+                      }
+                  }
+              });
+          });
+      </script>
 
   </head>
 
